7ce4823525eb380cc9bc7f1b183cf74f9051525d
Shireen Anees Ahmed <shireenbanu89@gmail.com>
2026-02-14 20:35:27 -0800

    Region added

--- services/s3Service.js
+++ services/s3Service.js
@@ -5,7 +5,7 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import {logPerformance} from './splunkLogger.js';
 
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: "us-west-2"
   // credentials: {
   //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
